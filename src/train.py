35eef261701d7cca38916f058b7e54f6af1f81b9
RitikAgrawal01 <agrawalritik2001@gmail.com>
2026-03-06 16:07:59 +0530

    Try 13

--- src/train.py
+++ src/train.py
@@ -111,7 +111,7 @@ def parse_arguments():
 
     # ---- Data ----
     parser.add_argument("--val_split",
-                        type=float, default=0.01,
+                        type=float, default=0.001,
                         help="Fraction of training data for validation (default: 0.1)")
 
     # ---- Fixed architecture I/O (usually don't change) ----
